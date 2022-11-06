c283900c0529f64cef1746fe501d6bb32b04d2f2
Marcio Duarte <marcioadr88@gmail.com>
2022-11-05 21:19:49 -0300

    initial xcode project

---
+++ Aire Libre/Aire_LibreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aire_LibreApp.swift
+//  Aire Libre
+//
+//  Created by Marcio Duarte on 2022-11-05.
+//
+
+import SwiftUI
+
+@main
+struct Aire_LibreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
