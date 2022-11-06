209a6f670074f3ab5b9700291cb611bb281a87d2
Marcio Duarte <marcioadr88@gmail.com>
2022-11-06 00:45:17 -0300

    add some models

---
+++ Aire Libre/Models/AQIData.swift
@@ -0,0 +1,23 @@
+//
+//  AQIData.swift
+//  Aire Libre
+//
+//  Created by Marcio Duarte on 2022-11-06.
+//
+
+import Foundation
+
+struct AQIData: Codable {
+    let sensor, source, description: String
+    let longitude, latitude: Double
+    let quality: Quality
+
+    enum CodingKeys: String, CodingKey {
+        case sensor
+        case source
+        case description
+        case longitude
+        case latitude
+        case quality
+    }
+}
---
+++ Aire Libre/Models/Quality.swift
@@ -0,0 +1,13 @@
+//
+//  Quality.swift
+//  Aire Libre
+//
+//  Created by Marcio Duarte on 2022-11-06.
+//
+
+import Foundation
+
+struct Quality: Codable {
+    let category: String
+    let index: Int
+}
